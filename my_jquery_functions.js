9ac993889ad7dcd2a51ef878960f1c713b0f9e96
Jenksz <zfiscr@gmail.com>
2014-01-19 17:18:03 -0500

    Personal Sandbox Site

---
+++ my_jquery_functions.js
@@ -0,0 +1,43 @@
+// my jQuery Functions
+<<<<<<< HEAD
+ $(document).ready(function(){
+		$("img").fadeIn(2500);
+		$("h1").fadeIn(2500);
+		$("#aboutme").fadeIn(2500);
+		$("#about").fadeIn(2500);
+
+});
+
+ /** Examples
+
+ 		$("li").first().next().text("1000");
+=======
+
+ $(document).ready(function(){
+ 	
+ 	/** Examples
+	$("li").first().next().text("1000");
+>>>>>>> 91fbbbbe707eebb7dff6590c7874336f71a586c9
+
+		$("a").click(function(event){
+			alert("Thanks for visiting!");
+			event.preventDefault();
+		});
+
+		$("a").addClass("test");
+
+		$("a").removeClass("test");
+<<<<<<< HEAD
+*/ 
+=======
+	
+
+		$("a").click(function(event) {
+			event.preventDefault();
+			$(this).hide("fast");
+		});
+		
+		*/
+
+	});
+>>>>>>> 91fbbbbe707eebb7dff6590c7874336f71a586c9
