ef6c735f31346f57a272b139a44e92cdcb98c53a
Jenksz <zfiscr@gmail.com>
2014-01-19 13:24:30 -0500

    jQuery upload

---
+++ my_jquery_functions.js
@@ -0,0 +1,20 @@
+// my jQuery Functions
+
+ $(document).ready(function(){
+	$("li").first().next().text("1000");
+
+		$("a").click(function(event){
+			alert("Thanks for visiting!");
+			event.preventDefault();
+		});
+
+		$("a").addClass("test");
+
+		$("a").removeClass("test");
+
+		$("a").click(function(event) {
+			event.preventDefault();
+			$(this).hide("fast");
+		});
+
+	});
